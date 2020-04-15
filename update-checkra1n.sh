2badb639b862fa6c74fbe98a6175494f482ccec9
r-carlo <63750646+r-carlo@users.noreply.github.com>
2020-04-15 14:45:53 -0700

    Update update-checkra1n.sh

--- update-checkra1n.sh
+++ update-checkra1n.sh
@@ -3,8 +3,8 @@ sudo apt install -y libc6 libncurses5 libpango-1.0-0 libpangocairo-1.0-0 libpang
 sudo rm -f checkra1n
 sudo rm -f checkra1n-armel
 sudo wget https://assets.checkra.in/downloads/linux/cli/arm/1ed46fb34c72ee942a1af185fd5347b999f4bba667d594dd56554e3b3d3ea417/checkra1n-armel -P /home/pi
-sudo cp /home/pi/checkra1n-armel /home/pi/checkra1n
-sudo rm /home/pi/checkra1n-armel
+sudo cp checkra1n-armel checkra1n
+sudo rm checkra1n-armel
 sudo chmod +x checkra1n
 echo Checkra1n Installed/updated! 
 echo Use 'sudo ./checkra1n' to run checkra1n.
