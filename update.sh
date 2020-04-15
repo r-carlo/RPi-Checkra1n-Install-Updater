6bbd2f97cde37895a6be77fbce654766e5c537e3
r-carlo <63750646+r-carlo@users.noreply.github.com>
2020-04-15 15:09:49 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ sudo rm -f checkra1n-armel
 sudo wget https://assets.checkra.in/downloads/linux/cli/arm/1ed46fb34c72ee942a1af185fd5347b999f4bba667d594dd56554e3b3d3ea417/checkra1n-armel -P /home/pi
 sudo cp checkra1n-armel checkra1n
 sudo rm checkra1n-armel
-sudo chmod +x /home/pi/checkra1n
+sudo chmod +x checkra1n
 echo Checkra1n successfully installed/updated!
 echo Use 'sudo ./checkra1n' to execute checkra1n.
 
