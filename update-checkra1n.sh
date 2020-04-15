30107c64ef09ff3852d930414586264497c8826f
r-carlo <63750646+r-carlo@users.noreply.github.com>
2020-04-15 13:38:24 -0700

    Update update-checkra1n.sh

--- update-checkra1n.sh
+++ update-checkra1n.sh
@@ -1,5 +1,4 @@
 sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade
-sudo apt install -y libc6 libncurses5 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libatk1.0-0 libgdk-pixbuf2.0-0 libglib2.0-0 libfontconfig1 libfreetype6 libgtk-3-0 libusb-1.0-0 libplist3 usbmuxd
 sudo rm -f checkra1n
 sudo rm -f checkra1n-armel
 sudo wget https://assets.checkra.in/downloads/linux/cli/arm/1ed46fb34c72ee942a1af185fd5347b999f4bba667d594dd56554e3b3d3ea417/checkra1n-armel
